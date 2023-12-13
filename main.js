c921242bb1de1caa03f1e54dfd079362ce105e7d
francis <58594771+LUCASFRANKINC@users.noreply.github.com>
2023-12-13 17:27:06 +0300

    Update main.js to handle double operand insertion error

--- main.js
+++ main.js
@@ -37,24 +37,13 @@ document.querySelectorAll(".number").forEach((number) => {
 document.querySelectorAll(".operator").forEach((operator) => {
   operator.addEventListener("click", (event) => {
     str += event.target.innerHTML;
-    //console.log(event.target.value)
+    const secondLastChar = str[str.length - 2];
+    // check if the last character is an operator
+    if (secondLastChar === "+" || secondLastChar === "-" || secondLastChar === "*" || secondLastChar === "/") {
+      str = str.slice(0, -2);
+      str += event.target.innerHTML;
+    }
+    
     document.querySelector("input").value = str;
   });
 });
-
-/* 
-del.addEventListener('click', function (event) {
-  if (event.target.innerHTML == 'DEL') {
-    str = str.slice(0, -1)
-    document.querySelector('input').value = str;
-  }
-}) */
-/* 
-clear.addEventListener('click', function (event) {
-  if (event.target.innerHTML == 'AC') {
-    str = '';
-    document.querySelector('input').value = str;
-  }
-}) */
-
-// blah blah blah blah bluubuu
